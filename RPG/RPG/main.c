d9b9fbda2f2edb85ee441938d0ffd7a4b2e0addc
Lee jiwon <leejiwon0615@naver.com>
2024-12-02 13:40:34 +0900

    third commit : Gotoxy, Cursorview add
    마우스 커서를 사용자가 조절하는 메소드와 커서 보임 여부를 추가

--- RPG/RPG/main.c
+++ RPG/RPG/main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <conio.h>
+#include <stdbool.h>
 
 #define MENU_SELECT_ONE 49		// 시작 메뉴 선택항목 1~4번까지의 아스키코드
 #define MENU_SELECT_TWO 50
 #define MENU_SELECT_THREE 51
 #define MENU_SELECT_FOUR 52
 
+
 void StartMenu();
+void Gotoxy(int x, int y); // 마우스 커서 위치를 변경하는 함수
+void CursorView(char show);
 
 int main(void) {
 
 
-	while (1) {
+	while (true) {
 		StartMenu();
 		int menu_select = _getch();	// 입력받는 키보드값 저장
 		system("cls");
@@ -41,4 +45,16 @@ void StartMenu() {
 	printf("                           4. 종 료                                \n\n");
 	printf("                 (게임시작전 도움말을 꼭 읽어주세요.)                \n\n\n");
 	printf("===================================================================");
+}
+void Gotoxy(int x, int y) {		// 커서 위치
+	COORD Pos;
+	Pos.X = x;
+	Pos.Y = y;
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
+void CursorView(char show) {	// 커서 보임 유무
+	CONSOLE_CURSOR_INFO ConsoleCursor;
+	ConsoleCursor.bVisible = show;
+	ConsoleCursor.dwSize = 1;
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &ConsoleCursor);
 }
